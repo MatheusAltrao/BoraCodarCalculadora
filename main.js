74493878abba6e127710c76b72f0765082c8d262
Matheus Altrao <matheusaltrao@MacBook-Pro-de-Matheus.local>
2023-02-03 20:29:47 -0300

    add button copy

--- main.js
+++ main.js
@@ -1,33 +1,52 @@
 let input = document.getElementById('countOperation')
 let result = document.getElementById('result')
-
+const copy = document.querySelector('.copy')
 
 document.querySelectorAll('.charKey').forEach(function (charBtn) {
   charBtn.addEventListener('click', () => {
     const value = charBtn.dataset.value
-    input.innerText += value
+    input.value += value
   })
 })
 
 document.getElementById('clear').addEventListener('click', () => {
-  input.innerText = ''
+  input.value = ''
   result.innerText = ''
+  copy.classList.remove('active')
+  result.classList.remove('active')
+
 })
 
 document.getElementById('back').addEventListener('click', backspace)
 
 document.getElementById('equal').addEventListener('click', calculator)
 
+copy.addEventListener('click', copyResult)
+
 function backspace() {
-  let resultado = document.getElementById('countOperation').innerHTML
-  document.getElementById('countOperation').innerHTML = resultado.substring(
+  let resultado = document.getElementById('countOperation').value
+  document.getElementById('countOperation').value = resultado.substring(
     0,
     resultado.length - 1
   )
 }
 
 function calculator() {
-  const countResult = parseInt(eval(input.innerText))
+  const countResult = eval(input.value)
+
   result.innerText = countResult
-  input.innerText = ''
+  input.value = ''
 }
+
+function copyResult() {
+  if (result.innerText.length === 0) {
+    copy.classList.remove('active')
+  } else {
+    copy.classList.add('active')
+    result.classList.add('active')
+  }
+
+  navigator.clipboard.writeText(result.innerText);
+}
+
+
